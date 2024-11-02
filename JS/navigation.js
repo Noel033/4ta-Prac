a4ece04cee6257400da7f8e0b1d51784b81a57e4
Antibush <noeljefry71@gmail.com>
2024-11-02 08:36:56 -0500

    ahora as

--- JS/navigation.js
+++ JS/navigation.js
@@ -1,28 +1,29 @@
 const Navigation = () => {
   return (
     <div>
-      <header className="p-4">
-        <div className="container mx-auto flex flex-col md:flex-row justify-between items-center gap-4">
-          <div className="flex items-center gap-4"
-           href="index.html">
-            <img
-              src="https://cdn.oddspedia.bg/media/es/1x1/sport-dota-2.jpg"
-              alt="Logo"
-              className="navbar-logo h-12 md:h-16 w-auto"
-            />
-            <h1 className="text-xl md:text-2xl font-bold">
-              La Guarida de Antibush
-            </h1>
-          </div>
-          <div className="relative w-full md:w-auto">
-            <input
-              type="search"
-              placeholder="¿Qué estás buscando?"
-              className="w-full md:w-64 px-4 py-2 rounded-lg bg-gray-700 text-white"
-            />
-          </div>
-        </div>
-      </header>
+     <header className="p-4">
+  <div className="container mx-auto flex flex-col md:flex-row justify-between items-center gap-4">
+    <div className="flex items-center gap-4">
+      <a href="index.html">
+        <img
+          src="https://cdn.oddspedia.bg/media/es/1x1/sport-dota-2.jpg"
+          alt="Logo"
+          className="navbar-logo h-12 md:h-16 w-auto"
+        />
+      </a>
+      <h1 className="text-xl md:text-2xl font-bold">
+        La Guarida de Antibush
+      </h1>
+    </div>
+    <div className="relative w-full md:w-auto">
+      <input
+        type="search"
+        placeholder="¿Qué estás buscando?"
+        className="w-full md:w-64 px-4 py-2 rounded-lg bg-gray-700 text-white"
+      />
+    </div>
+  </div>
+</header>
       <nav className="bg-gray-800">
         <ul className="flex flex-wrap justify-center md:justify-around space-x-2 md:space-x-4 p-4">
           <li className="my-1">
